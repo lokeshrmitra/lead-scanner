70a373ecc9d94082a235cf23a21a698b8fc87163
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2018-10-04 23:43:57 +0530

    Adding image upload form

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
 const express = require("express");
+const multer = require("multer");
+const ejs = require("ejs");
+const path = require("path");
+
+//Set Storage Engine
+const storage = multer.diskStorage({
+  destination: "./public/uploads",
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  }
+});
+
+//Init Upload
+const upload = multer({
+  storage: storage
+  //   limits: { fileSize: 10 }
+}).single("avatar");
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
-  res.send("This is me");
+  res.render("index");
 });
 
+app.post("/upload", (req, res) => {
+  upload(req, res, err => {
+    if (err) res.render("index", { msg: err });
+    else {
+      if (req.file == undefined) {
+        res.render("index", {
+          msg: "No File uploaded"
+        });
+      } else {
+        res.render("index", {
+          msg: "file uploaded",
+          file: `uploads/${req.file.filename}`
+        });
+      }
+    }
+  });
+});
+
+const port = process.env.port || 3000;
+
 app.listen(port, () => {
-  console.log("Server running on 3000");
+  console.log("Server running on " + port);
 });
