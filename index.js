daf4cea00aa1c13ca2cf3d848d01fd50331fe445
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2018-10-04 23:51:53 +0530

    Adding env port

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post("/upload", (req, res) => {
   });
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log("Server running on " + port);
